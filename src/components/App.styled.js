15cc62fad5659534cc74989b68a7f132a38cca8a
Volodymyr <vladikon70@gmail.com>
2023-10-05 22:31:49 +0200

    update files

--- src/components/App.styled.js
+++ src/components/App.styled.js
@@ -1,10 +1,32 @@
 import styled from 'styled-components';
+import background from '../images/funart.jpg';
 
 export const Layout = styled.div`
   display: grid;
   grid-template-columns: 1fr;
   /* grid-gap: 16px; */
   padding-bottom: 24px;
+  align-content: space-between;
+  &:before {
+    content: '';
+    background-image: linear-gradient(
+        rgba(106, 191, 235, 0),
+        rgba(106, 191, 235, 1)
+      ),
+      url(${background});
+    background-blend-mode: multiply;
+    background-size: cover;
+    background-position: center;
+    background-repeat: no-repeat;
+    position: fixed;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    z-index: -5;
+    opacity: 0.6;
+    filter: blur(1px);
+  }
 `;
 
 export const Loading = styled.div`
